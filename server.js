bc13e53bec027db5449fca36d438a9ccb9104124
Abstractmelon <abstractmelons@gmail.com>
2025-03-15 09:38:40 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-app.use(express.static("./"));
+// Middleware to serve static files
+app.use(express.static(path.join(__dirname)));
 
+// Route to serve the main HTML file
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
